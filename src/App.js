e9b834439f20f9647c75a7492bd0e63711f6b6d7
Steven Black <genseb7@gmail.com>
2020-08-02 11:20:51 -0400

    fis state bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-
 const ReactMarkdown = require('react-markdown')
 
 const input = '# This is a header\n\nAnd this is a paragraph'
 
 class App extends Component {
+  state = { data: '' }
+  constructor(props) {
+    super (props)
+  }
+
   async getData() {
     const res = await fetch("hello-world.md");
     const data = await res.text();
@@ -15,9 +18,7 @@ class App extends Component {
     return this.setState({ data });
   }
 
-  state = await this.getData() 
-
-  componentWillMount() {
+  componentDidMount() {
     this.getData();
   }
 
