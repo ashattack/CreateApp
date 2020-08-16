88a40a34ddd8a794a841a791e534531de7b4fab9
ashattack <62859233+ashattack@users.noreply.github.com>
2020-08-16 00:00:58 -0400

    add static

---
+++ docs/precache-manifest.6825cf6eb52c5e7f5ca9ba04b193e5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2613f1f75992749995bf01e0bec5b1da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f48ad30a18a0e3d9c92f",
+    "url": "/static/css/main.292228b4.chunk.css"
+  },
+  {
+    "revision": "ae1a9c7dc14cda389bb4",
+    "url": "/static/js/2.7d45c8e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d45c8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f48ad30a18a0e3d9c92f",
+    "url": "/static/js/main.deb5e36b.chunk.js"
+  },
+  {
+    "revision": "74773a127c631889e6ec",
+    "url": "/static/js/runtime-main.b368757b.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 
 import './App.css';
 import React, { Component } from 'react';
-const ReactMarkdown = require('react-markdown')
-
-const input = '# This is a header\n\nAnd this is a paragraph'
 
 class App extends Component {
-  state = { data: '' }
-  constructor(props) {
-    super (props)
-  }
-
-  async getData() {
-    const res = await fetch("hello-world.md");
-    const data = await res.text();
-    console.log(data);
-    return this.setState({ data });
-  }
-
-  componentDidMount() {
-    this.getData();
-  }
 
   render() {
     return (
       <div className="App">
+
       <header className="App-header" >
         <h1>Ashlyn Hicks</h1> 
-      <h3> Web Developer</h3>
+        <h3> Web Developer</h3>
       </header>
+
+      <div className="page">
+        <h2>About Me</h2>
+        <p>I popped outta the womb in Baltimore, Maryland where I ran free as a younggin. Florida is where I spent
+          of my years, acting all cool, playing some b-ball at my school. I grew bored with the dull ways of an American
+          life and decided I needed drop everything and move to Europe at the ripe age of 16. With the help of Interact Club and the
+          Rotary Organization, I packed my bags and headed to the Faroe Islands. I spent a year here, frollicking in the fields and
+          climbing mountains. I continued my studies but this time it wasn't in English, I had learned Faroese. When I came back
+          to Florda I finished school, received my A.A degree in business and started teaching myself programming. Also, on another note,
+          my love language is Words of Affirmations. Please don't forget it. :) </p>
+      </div>
+
       <div className ="page" >
         <h2>Fork me on Github!</h2>
         <p>My Github handle is: <a href="https://github.com/ashattack">@ashattack </a> </p>
         <div id="github-contributions"></div>
         <div id="github-language-distribution" ></div>
       </div>
-      <ReactMarkdown source={this.state.data} />
+
+      <div className="page">
+        <h2>foof</h2>
+        <div className="github-card" data-github="ashattack" data-width="400" data-height="150" data-theme="default"></div>
+        <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
+      </div>
+      
+      <div id="footer"></div>
     </div>
     );
   }
